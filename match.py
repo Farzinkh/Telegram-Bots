16ec83a0cbb56a1cc5985242a15faee58c97914d
magcofen <farzin.mag@gmail.com>
2020-03-15 13:32:33 +0330

    Update match.py

--- match.py
+++ match.py
@@ -27,22 +27,23 @@ def sql_connection():
 def sql_table():
 	global db
 	courobj=db.cursor()
-	courobj.execute("CREATE TABLE merrymaker(username text,firstname text,lastname text,phonenumber real,point real,state text)")
+	courobj.execute("CREATE TABLE merrymaker(ID INT PRIMARY KEY,username text,firstname text,lastname text,phonenumber real,point real,state text)")
 	db.commit()
 def sql_insert(data):
 	global db
 	courobj=db.cursor()
-	courobj.execute("""INSERT INTO merrymaker(username,firstname,lastname,phonenumber,point,state) VALUES(?,?,?,?,?,?)""",data)
+	courobj.execute("""INSERT INTO merrymaker(ID,username,firstname,lastname,phonenumber,point,state) VALUES(?,?,?,?,?,?,?)""",data)
 	db.commit()
 def sql_update(key,data,username):
-	global db
+	global db,identifylist
 	courobj=db.cursor()
-	courobj.execute("UPDATE merrymaker SET {}={} where username={}".format(key,data,username))
+	courobj.execute("UPDATE merrymaker SET {}={} where id={}".format(key,data,identifylist[username]))
 	db.commit()
-global db
+global db,idnum,identifylist
 db=sql_connection()
 sql_table()
-
+idnum=1
+identifylist={}
 global answerlist,questionsdoc,questionlist
 lines=open('questions.txt','r').readlines()
 count,countans,answerlist,questionsdoc,questionlist=0,5,[],{},[]
@@ -86,8 +87,9 @@ def makeKeyboard(stringList):
 @bot.message_handler(commands=['end'])
 def surrend(message):
 	try:
-		sql_update('state','finish',message.from_user.username)
+		sighuplist[message.from_user.username]['state']='finish'
 		bot.reply_to(message, 'oh my friend {} it was so hard for you,i khow'.format(message.from_user.first_name))
+		sql_update('state','finish',message.from_user.username)
 	except:
 		print("there is problem in database",message.from_user.username)
 		pass
@@ -155,7 +157,9 @@ def sighup(message):
 				bot.send_message(message.chat.id, " Ok now enter your first name :")
 				sighuplist[message.from_user.username]={'match':'on','firstname':'','lastname':'','phonenumber':message.text,'questionnumbers':[],'stringList':'stringList','state':'alive','point':0,'questionnum':0,'timer':time.time(),'chat_id':message.chat.id}
 				sighuplist[message.from_user.username]['stringList']=makequestion(message)
-				z=(sighuplist[message.from_user.username],sighuplist[message.from_user.username]['firstname'],sighuplist[message.from_user.username]['lastname'],sighuplist[message.from_user.username]['phonenumber'],sighuplist[message.from_user.username]['point'],sighuplist[message.from_user.username]['state'])
+				idnum+=1
+				identifylist[message.from_user.username]=idnum
+				z=(idnum,sighuplist[message.from_user.username],sighuplist[message.from_user.username]['firstname'],sighuplist[message.from_user.username]['lastname'],sighuplist[message.from_user.username]['phonenumber'],sighuplist[message.from_user.username]['point'],sighuplist[message.from_user.username]['state'])
 				sql_insert(z)
 		except:
 			pass
